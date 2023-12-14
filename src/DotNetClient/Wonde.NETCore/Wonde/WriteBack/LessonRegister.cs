05fa0a2484c4cd6c43fd9a2875afd91f93896b72
richard-xello <137922901+richard-xello@users.noreply.github.com>
2023-12-14 14:05:53 -0800

    Create .NET Standard project form Wonde.NET

---
+++ src/DotNetClient/Wonde.NETCore/Wonde/WriteBack/LessonRegister.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using Wonde.Exceptions;
+
+namespace Wonde.WriteBack
+{
+    public class LessonRegister
+    {
+        /// <summary>
+        /// Attendance array list
+        /// </summary>
+        public List<LessionAttendanceRecord> attendance;
+
+        /// <summary>
+        /// LessionRegister constructor
+        /// </summary>
+        public LessonRegister()
+        {
+            attendance = new List<LessionAttendanceRecord>();
+        }
+
+        /// <summary>
+        /// Add LessionAttendanceRecord to the attendance array list
+        /// </summary>
+        /// <param name="lessionAttendance">Object of LessionAttendanceRecord</param>
+        /// <exception cref="InvalidLessonAttendanceException" />
+        public void add(LessionAttendanceRecord lessionAttendance)
+        {
+            if (lessionAttendance == null)
+                throw new InvalidLessonAttendanceException("Attendance is null or is not an instance of the LessonAttendanceRedord Class.");
+
+            if (!lessionAttendance.isValid())
+                throw new InvalidLessonAttendanceException("Attendance has empty fields.");
+
+            attendance.Add(lessionAttendance);
+        }
+    }
+}
